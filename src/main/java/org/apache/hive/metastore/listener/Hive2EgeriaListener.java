987f52962fbeb2c923cc0dfb2a0e2b242ee4e406
William Bittles <wbittles@uk.ibm.com>
2023-01-09 16:56:15 +0000

    add exception handler
    Signed-off-by: William Bittles <wbittles@uk.ibm.com>

--- src/main/java/org/apache/hive/metastore/listener/Hive2EgeriaListener.java
+++ src/main/java/org/apache/hive/metastore/listener/Hive2EgeriaListener.java
@@ -15,6 +15,8 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(Hive2EgeriaListener.class);
 
+    private static final String PROPOGATE_EXCEPTIONS = "org.apache.hive.metastore.listener.EgeriaListener.events.exceptions";
+
     public Hive2EgeriaListener(Configuration config) {
         super(config);
 
@@ -23,6 +25,8 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
     }
 
     /**
+     * debugging when this event is triggered
+     * and if we can use them for hot swapping
      * @param tableEvent table event.
      * @throws MetaException
      */
@@ -31,6 +35,25 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
         super.onConfigChange(tableEvent);
         if(logger.isDebugEnabled())
             logger.debug(String.format("onConfigChange : %s ", tableEvent.toString()));
+    }
+
+
+    /**
+     *
+     * @param msg       the text to be displayed in the error log
+     * @param ex        the original exception
+     * @throws MetaException
+     */
+    private void handleException( String msg, Exception ex) throws MetaException {
+        /*
+        never hide the fact that exceptions are being thrown .
+        just don't let HMS know
+         */
+        logger.error(msg, ex);
+        if( getConf().get(PROPOGATE_EXCEPTIONS,"off").equalsIgnoreCase("on")) {
+            throw new MetaException( ex.getMessage() );
+        }
+
     }
 
 }
