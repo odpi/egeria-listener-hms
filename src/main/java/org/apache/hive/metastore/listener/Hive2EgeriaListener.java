ece84a830bcb8e47c46931030ce0735a40375c5b
William Bittles <wbittles@uk.ibm.com>
2023-01-10 15:24:59 +0000

    add create database event handler
    Signed-off-by: William Bittles <wbittles@uk.ibm.com>

--- src/main/java/org/apache/hive/metastore/listener/Hive2EgeriaListener.java
+++ src/main/java/org/apache/hive/metastore/listener/Hive2EgeriaListener.java
@@ -15,7 +15,19 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(Hive2EgeriaListener.class);
 
+    /*
+    property values
+     */
+    private static final String ON = "on";
+    private static final String OFF = "off";
+
+    /*
+    property names
+     */
     private static final String PROPOGATE_EXCEPTIONS = "org.apache.hive.metastore.listener.EgeriaListener.events.exceptions";
+    private static final String HOT_SWAP = "org.apache.hive.metastore.listener.EgeriaListener.events.active.hotswap";
+    private static final String ACTIVE = "org.apache.hive.metastore.listener.EgeriaListener.events.active";
+    private static final String MAPPER = "org.apache.hive.metastore.listener.EgeriaListener.events.mapper";
 
     public Hive2EgeriaListener(Configuration config) {
         super(config);
@@ -25,6 +37,28 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
     }
 
     /**
+     * @param dbEvent database event
+     * @throws MetaException
+     */
+    @Override
+    public void onCreateDatabase(CreateDatabaseEvent dbEvent) throws MetaException {
+        super.onCreateDatabase(dbEvent);
+
+        if(logger.isDebugEnabled())
+            logger.debug(String.format("onCreateDatabase %n%s", dbEvent.toString()));
+
+        if( getConf().get(HOT_SWAP, OFF).equalsIgnoreCase(ON) ) {
+            getConf().reloadConfiguration();
+        }
+
+        if( getConf().get(ACTIVE,OFF).equalsIgnoreCase(ON)) {
+            postEvent( dbEvent.getDatabase());
+        }
+
+    }
+
+
+    /**
      * debugging when this event is triggered
      * and if we can use them for hot swapping
      * @param tableEvent table event.
@@ -32,9 +66,10 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
      */
     @Override
     public void onConfigChange(ConfigChangeEvent tableEvent) throws MetaException {
-        super.onConfigChange(tableEvent);
-        if(logger.isDebugEnabled())
+        if(logger.isDebugEnabled()) {
             logger.debug(String.format("onConfigChange : %s ", tableEvent.toString()));
+        }
+        super.onConfigChange(tableEvent);
     }
 
 
@@ -45,13 +80,49 @@ public class Hive2EgeriaListener extends MetaStoreEventListener {
      * @throws MetaException
      */
     private void handleException( String msg, Exception ex) throws MetaException {
+
+        if( logger.isDebugEnabled()) {
+            logger.debug( String.format("handleException %n%s%n%s",msg,ex.getMessage()));
+        }
         /*
         never hide the fact that exceptions are being thrown .
-        just don't let HMS know
+        just decide if we let HMS know
          */
         logger.error(msg, ex);
-        if( getConf().get(PROPOGATE_EXCEPTIONS,"off").equalsIgnoreCase("on")) {
+        if( getConf().get(HOT_SWAP, OFF).equalsIgnoreCase(ON)) {
+            getConf().reloadConfiguration();
+        }
+        if( getConf().get(PROPOGATE_EXCEPTIONS,OFF).equalsIgnoreCase(ON)) {
             throw new MetaException( ex.getMessage() );
+        }
+
+    }
+
+
+    /**
+     *
+     * @param event       the bean transformed into the JSON to be posted to kafka
+
+     * @throws MetaException
+     */
+    private void postEvent( Object event) throws MetaException {
+
+        if( logger.isDebugEnabled()) {
+            logger.debug( String.format("postEvent %n%s", event.toString()));
+        }
+
+        ObjectMapper objMapper = new ObjectMapper();
+        try {
+            String json = objMapper.writeValueAsString(event);
+
+            /*
+            posting to kafka to be added later
+             */
+
+            System.out.println( String.format("Event %n%s",json) );
+
+        } catch (Exception ex) {
+            handleException("An Exception was thrown mapping an event to json", ex);
         }
 
     }
